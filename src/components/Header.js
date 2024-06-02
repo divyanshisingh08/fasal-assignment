2daca437305a332acf5fd72c34f1e08e63f9e0d2
divyanshi singh <divyanshisingh2308@gmail.com>
2024-06-02 16:29:54 +0530

    Sign In and Sign Up Form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo from './images/logo.png'
+import logo1 from './images/logo1.png'
 
 
 const Header = () => {
   return (
  
     <div className=' absolute px-8 py-2  bg-gradient-to-b from-black z-10'>
-     <img className='w-16 h-16' src={logo} alt="logo" />
+     <img className='w-16 h-16' src={logo1} alt="logo" />
     </div>
     
   )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,29 @@
 import React from "react";
 import Header from "./Header";
 import { BACKGROUND_URL } from "../utils/constants";
+// import bg from './images/bg.jpg'
+import { useState } from "react";
 
 const Login = () => {
+
+  const [isSignInForm,SetIsSignInForm]=useState(true)
+
+  const toggleSignInForm=()=>{
+    SetIsSignInForm(!isSignInForm) ;
+  }
   return (
     <div >
       <Header />
       <div>
-        <img className="absolute" src={BACKGROUND_URL} alt="bg" />
+        <img className="absolute " src={BACKGROUND_URL} alt="bg" />
       </div>
-      <form className="w-2/6 absolute p-12 bg-white my-28 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-90">
-        <h1 className="font-bold text-3xl py-4 text-cyan-900">Sign In </h1>
+      <form className="w-2/6 absolute p-12 bg-white my-24 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-90">
+        <h1 className="font-bold text-3xl py-2 text-cyan-900">{isSignInForm?"Sign In": "Sign Up" }</h1>
+      { !isSignInForm && <input
+          type="text"
+          placeholder="Full Name"
+          className="p-4 my-4 w-full bg-slate-300 text-xl"
+        />}
         <input
           type="text"
           placeholder="Email Address"
@@ -21,7 +34,8 @@ const Login = () => {
           placeholder="Password"
           className="p-4 my-4 w-full bg-slate-300 text-xl"
         />
-        <button className=" p-4 my-4 w-full bg-cyan-900 text-2xl">Sign In</button>
+        <button className=" p-4 my-4 w-full bg-cyan-900 text-2xl">{isSignInForm?"Sign In": "Sign Up" }</button>
+        <p className="py-2 text-cyan-900 font-bold text-lg cursor-pointer" onClick={toggleSignInForm}>{isSignInForm?"New To Fasal? Sign Up Now": "Already Registered? Sign In Now" }</p>
       </form>
     </div>
   );
